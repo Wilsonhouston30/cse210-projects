36d3f4b84048efcf60522b6aa110e245f0d683c3
Wilsonhouston30 <hou22004@byui.edu>
2023-11-14 02:04:16 -0600

    W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._title = "Software Engineer";
+        job1._startYear = 2000;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Adobe";
+        job2._title = "Janitor";
+        job2._startYear = 1999;
+        job2._endYear = 2000;
+
+        Console.WriteLine();
+
+        Resume myResume = new Resume();
+        myResume._name = "Wilson Houston";
+
+
+
+
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
+
     }
 }
