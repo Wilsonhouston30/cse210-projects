2f964ea2d9d1812bc1160d508f4178f8e835a551
Wilsonhouston30 <hou22004@byui.edu>
2025-04-22 22:10:46 -0600

    Completed Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter class grade percentage: ");
+        var userInput = Int32.Parse(Console.ReadLine());
+
+        string grade = "";
+
+        if (userInput >= 90)
+        {
+            grade = "A";
+        }
+        else if (userInput >= 80)
+        {
+            grade = "B";
+        }
+        else if (userInput >= 70)
+        {
+            grade = "C";
+        }
+        else if (userInput >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Letter Grade: {grade}");
     }
 }
